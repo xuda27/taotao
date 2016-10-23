72206852280c0339705ff90a40c7d167ae5a274c
xuda <xuda27@163.com>
2016-10-23 17:05:18 +0800

    test ftp

---
+++ taotao-common/src/test/java/cn/eden/taotao/test/FtpTest.java
@@ -0,0 +1,35 @@
+package cn.eden.taotao.test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.SocketException;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.junit.Test;
+
+/**
+ * 使用commons-net包 测试ftp服务器
+ * @author Eden
+ *
+ */
+public class FtpTest {
+	@Test
+	public void testFtp() throws Exception {
+		//1. 连接ftp服务器
+		FTPClient ftpClient = new FTPClient();
+		ftpClient.connect("123.207.27.28", 21);
+		//2. 登录ftp服务器
+		ftpClient.login("ftpuser", "a");
+		//3. 读取本地文件
+		FileInputStream fis = new FileInputStream("D:\\images\\1.jpg");
+		//4. 上传文件
+		//指定上传目录
+		ftpClient.changeWorkingDirectory("/home/ftpuser/www/images");
+		//指定上传类型
+		ftpClient.setFileType(FTPClient.BINARY_FILE_TYPE);
+		ftpClient.storeFile("girl.jpg", fis);
+		//5.退出登录
+		ftpClient.logout();
+		
+	}
+}
